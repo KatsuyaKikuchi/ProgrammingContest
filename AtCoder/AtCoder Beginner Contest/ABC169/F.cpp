793694dfaf5b9f02a94d0b527eb1c8a75e800a4b
Katsuya Kikuchi <k.kikuchi.ah@gmail.com>
2021-02-21 16:56:03 +0900

    solve problems

--- AtCoder/AtCoder Beginner Contest/ABC169/F.cpp
+++ AtCoder/AtCoder Beginner Contest/ABC169/F.cpp
@@ -12,47 +12,96 @@ typedef pair<ll, ll> pll;
 const ll MOD = 998244353;
 const ll INF = (ll) 1e15;
 
-ll A[3005];
-ll dp[3005][3005];
+template<std::uint_fast64_t Modulus>
+class ModInt {
+    using u64 = std::uint_fast64_t;
+public:
+    constexpr ModInt(u64 value = 0) noexcept:
+            mValue(value % Modulus) {}
 
-ll modpow(ll n, ll r) {
-    ll ret = 1;
-    ll p = n;
-    while (r > 0) {
-        if (r & 1)
-            ret = (ret * p) % MOD;
-        p = (p * p) % MOD;
-        r >>= 1;
+    u64 &value() { return mValue; }
+
+    constexpr ModInt &operator+=(ModInt v) noexcept {
+        mValue += v.mValue;
+        if (mValue >= Modulus)
+            mValue -= Modulus;
+        return *this;
     }
-    return ret;
-}
+
+    constexpr ModInt &operator-=(ModInt v) noexcept {
+        if (mValue < v.mValue)
+            mValue += Modulus;
+        mValue -= v.mValue;
+        return *this;
+    }
+
+    constexpr ModInt &operator*=(ModInt v) noexcept {
+        mValue = (mValue * v.mValue) % Modulus;
+        return *this;
+    }
+
+    constexpr ModInt &operator/=(ModInt v) noexcept {
+        *this *= v.pow(Modulus - 2);
+        return *this;
+    }
+
+    constexpr ModInt operator+(ModInt v) noexcept {
+        return ModInt(*this) += v;
+    }
+
+    constexpr ModInt operator-(ModInt v) noexcept {
+        return ModInt(*this) -= v;
+    }
+
+    constexpr ModInt operator*(ModInt v) noexcept {
+        return ModInt(*this) *= v;
+    }
+
+    constexpr ModInt operator/(ModInt v) noexcept {
+        return ModInt(*this) /= v;
+    }
+
+    ModInt pow(u64 r) {
+        u64 p = mValue;
+        ModInt ret(1);
+        while (r > 0) {
+            if (r & 1)
+                ret *= p;
+            r >>= 1;
+            p = (p * p) % Modulus;
+        }
+        return ret;
+    }
+
+private:
+    u64 mValue;
+};
 
 int main() {
+    using mint = ModInt<MOD>;
     cin.tie(0);
     ios::sync_with_stdio(false);
     ll N, S;
     cin >> N >> S;
-
+    vector<ll> A(N);
     REP(i, N) {
         cin >> A[i];
     }
 
-    memset(dp, 0, sizeof(dp));
-    dp[0][0] = 1;
+    vector<vector<mint>> dp(N + 1, vector<mint>(S + 1, 0));
+    dp[0][0] = ((mint) 2).pow(N);
+    mint div = ((mint) 2).pow(MOD - 2);
     REP(i, N) {
         REP(j, S + 1) {
-            dp[i + 1][j] = (dp[i + 1][j] + dp[i][j]) % MOD;
-            if (j + A[i] <= S)
-                dp[i + 1][j + A[i]] = (dp[i + 1][j + A[i]] + dp[i][j]) % MOD;
+            dp[i + 1][j] += dp[i][j];
+            if (dp[i][j].value() == 0)
+                continue;
+            ll nxt = j + A[i];
+            if (nxt > S)
+                continue;
+            dp[i + 1][nxt] += dp[i][j] * div;
         }
     }
-
-    ll ans = 0;
-    REP(i, N) {
-        cout << dp[i + 1][S] << endl;
-        ans += dp[i + 1][S] * modpow(2, N - (i - 1)) % MOD;
-        ans %= MOD;
-    }
-    cout << ans << endl;
+    cout << dp[N][S].value() << endl;
     return 0;
 }
